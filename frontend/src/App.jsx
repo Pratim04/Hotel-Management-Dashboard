0ea250c862930613463c1f29d7237c7b46f30605
Pratim04 <pratimhalder04@gmail.com>
2025-09-21 01:29:04 +0530

    first commit of HMD

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Barchart from "./components/Barchart";
+import Dataseries from "./components/Dataseries";
+import Navbar from "./components/Navbar";
+import Piechart from "./components/Piechart";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Sidebar/>  
+    {/* Other components can be added here */}  
+    </>
+  );
+}
+
+export default App
