5f3a7e391324f4c423bc061578ed722ea7bd660d
Pratim <169261223+Pratim04@users.noreply.github.com>
2025-12-20 11:19:55 +0530

    Test commit

--- frontend/src/components/Barchart.jsx
+++ frontend/src/components/Barchart.jsx
@@ -1,31 +1,20 @@
-import React from 'react'
-import { Chart as ChartJS } from "chart.js/auto";
-import { Bar } from 'react-chartjs-2';
-// import sourceData from "../Data/sourceData.json";
+import React from 'react';
+import sourceData from "../Data/sourceData.json";
+import { ResponsiveContainer ,Barchart ,Bar, XAxis, YAxis} from 'recharts';
 // import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-const Barchart = () => {
+const BarChart = () => {
     return (
         <>
-            <div className="h-60 w-100 rounded-xl shadow-lg bg-white dataCard m-2 p-2 customerCard">
-                <Bar
-                    data = {{
-                        labels: ["A", "B", "C"],
-                        datasets: [
-                            {
-                                label: "Revenue",
-                                data: [200, 300, 365],
-                            },
-                            {
-                                label: "Loss",
-                                data: [90, 80, 70],
-                            },
-                        ],
-                    }}
-                />
-            </div>
+            <ResponsiveContainer width= "50%" aspect={3}>
+                <Barchart data = {sourceData} width = {400} height = {400}>
+                    <XAxis dataKey="iteam"/>
+                    <YAxis />
+                    <Bar dataKey="price"/>
+                </Barchart>
+            </ResponsiveContainer>
         </>
     );
 }
 
-export default Barchart;
+export default BarChart;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import BarChart from '../components/Barchart';
+import LineChart from '../components/LineChart';
 
 function Dashboard() {
     return (
     <>
-        <h1>Dashboard</h1>
+        <BarChart/>
     </>
     )     
 }
