930ec180218e6bfe08a86fe676bc3655bfae1d2d
Pratim <169261223+Pratim04@users.noreply.github.com>
2025-12-25 00:12:26 +0530

    KPI added Bento Grid added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,51 +7,46 @@ import StatCard from '../components/Charts/KPI/StatCard';
 import DoughnutChart from '../components/Charts/Doughnutchart';
 function Dashboard() {
     return (
-        <div className='main'>
-            <div className='TR'>
-                <StatCard
-                    title="Total Revenue"
-                    value="$124,500"
-                    percentage={12.5}
-                    isPositive={true}
-                />
-            </div>
-            <div className='TU'>
-                <StatCard
-                    title="Total Orders"
-                    value="2,340"
-                    percentage={8.1}
-                    isPositive={true}
-                />
-            </div>
-            <div className='TL'>
-                <StatCard
-
-                    title="Total Loss"
-                    value="$18,200"
-                    percentage={-6.3}
-                    isPositive={false}
-                />
-            </div>
-            <div className="TN">
-                <StatCard 
-                    title="New User"
-                    value="56"
-                    percentage={15.3}
-                    isPositive={true}
-                />
-            </div>
-            <div className="barchart">
-                <Barchart />
-            </div>
-            <div className="linechart">
-                <Linechart />
-            </div>
-            <div className="piechart">
-                <Piechart />
-            </div>
-            <div className="doughnutchart">
-                <DoughnutChart />
+        <div className="wrapper">
+            <div className="gridLayout">
+                <div className="box box1">
+                    <Barchart />
+                </div>
+                <div className="box box2">
+                    <div className="wrap">
+                    <StatCard 
+                        className="TC"
+                        title="New User"
+                        value="56"
+                        percentage={15.3}
+                        isPositive={true}
+                        />
+                    <StatCard
+                        className="TR"
+                        title="Total Revenue"
+                        value="$56540"
+                        percentage={14.45}
+                        isPositive={true}
+                        />
+                    <StatCard
+                        className="TL"
+                        title="Loss"
+                        value="56"
+                        percentage={15.3}
+                        isPositive={true}
+                        />
+                    <StatCard
+                        className="TE"
+                        title="Employee"
+                        value="41"
+                        percentage={45}
+                        isPositive={true}
+                    />
+                    </div>
+                </div>
+                <div className="box box3"></div>
+                <div className="box box4"></div>
+                <div className="box box5"><Piechart /></div>
             </div>
         </div>
     );
